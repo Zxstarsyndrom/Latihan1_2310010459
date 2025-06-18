0f2a7b2f234b26cb9d104f9c5cb9d0ae72ef72d1
syarif <syarifridwan433@gmail.com>
2025-06-18 22:31:13 +0700

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -11,8 +11,23 @@ public class Matematika {
         return bil1 + bil2;
     }
 
+    public double setPengurangan() {
+        return bil1 - bil2;
+    }
+
+    public double setPerkalian() {
+        return bil1 * bil2;
+    }
+
+    public double setPembagian() {
+        return bil1 / bil2;
+    }
+
     public static void main(String[] args) {
         Matematika math = new Matematika(5.5, 3.2);
         System.out.println("Hasil Penjumlahan: " + math.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: " + math.setPengurangan());
+        System.out.println("Hasil Perkalian: " + math.setPerkalian());
+        System.out.println("Hasil Pembagian: " + math.setPembagian());
     }
 }

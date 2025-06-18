bed9fc174ca0b09a800f29c09371237caf68efef
syarif <syarifridwan433@gmail.com>
2025-06-18 22:30:28 +0700

    Membuat constructor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,4 +1,18 @@
 package hitung;
 public class Matematika {
-    
+    double bil1, bil2;
+
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+
+    public double setPenjumlahan() {
+        return bil1 + bil2;
+    }
+
+    public static void main(String[] args) {
+        Matematika math = new Matematika(5.5, 3.2);
+        System.out.println("Hasil Penjumlahan: " + math.setPenjumlahan());
+    }
 }
